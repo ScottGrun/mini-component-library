fe5a09ed7da0b8799494db94163ebd4ac7c5cbd4
ScottGrun <45321659+ScottGrun@users.noreply.github.com>
2021-04-14 15:54:10 -0400

    Added Input and Dropdown component

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,82 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+import { getDisplayedValue } from "./Select.helpers";
 
 const Select = ({ label, value, onChange, children }) => {
-  const displayedValue = getDisplayedValue(value, children);
+	const displayedValue = getDisplayedValue(value, children);
 
-  return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
-  );
+	return (
+		<DropdownWrapper>
+			<NativeDropdown value={value} onChange={onChange}>
+				{children}
+			</NativeDropdown>
+			<PresentationBit>
+				{displayedValue}
+				<IconWrapper>
+					<Icon id="chevron-down" size="24" strokeWidth="3" />
+				</IconWrapper>
+			</PresentationBit>
+		</DropdownWrapper>
+	);
 };
+
+const DropdownWrapper = styled.div`
+	position: relative;
+	width: max-content;
+
+	/* Prevent other dom elements z-index from interfering with this element */
+	isolation: isolate;
+`;
+
+// Hide the nativedropdown but keep it interactable
+const NativeDropdown = styled.select`
+	position: absolute;
+	top: 0;
+	left: 0;
+	opacity: 0;
+	width: 100%;
+	height: 100%;
+	z-index: 1;
+`;
+
+// This is the styled "dropdown" that you see
+const PresentationBit = styled.div`
+	background-color: ${COLORS.transparentGray15};
+	color: ${COLORS.gray700};
+	font-family: Roboto;
+	font-weight: normal;
+	font-size: 1rem;
+	line-height: 19px;
+	padding: 12px 16px;
+	padding-right: 52px;
+	border-radius: 8px;
+
+	/*
+   Okay this is neat, use the sibiling selector with the focus attribute 
+   to toggle the border around the presentation div 
+  */
+	${NativeDropdown}:focus + & {
+		outline: 1px dotted #212121;
+		outline: 5px auto -webkit-focus-ring-color;
+	}
+
+	${NativeDropdown}:hover + & {
+		color: ${COLORS.black};
+	}
+`;
+
+const IconWrapper = styled.div`
+	position: absolute;
+	top: 0;
+	bottom: 0;
+	right: 6px;
+	height: 24px;
+	width: 24px;
+	margin: auto;
+	pointer-events: none;
+`;
 
 export default Select;
